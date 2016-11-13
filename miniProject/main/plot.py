d628429a0655b448cfdfbbbc1f22cc941fb22c8f
jayd <7077804@qq.com>
2016-11-14 00:56:07 +0800

    adjust some file

--- miniProject/main/plot.py
+++ miniProject/main/plot.py
@@ -1,36 +1,14 @@
-import os 
-import re
-import sys
-import matplotlib.pyplot as plt
+f = open("The Monkey's Paw.txt")
+content = f.read()
+lines = content.strip().split();
+num = [int(x) for x in lines]
 
-Dir = "/home/jayd/Desktop/NUS/Combinatorial and Graph Algorithms/miniProject"
-# load book names in sub dir.
-subDirs = os.listdir(Dir)
-subDirs = sorted(subDirs)
-booksName = []
-booksDir = []
-for d in subDirs:
-    if d[:5] == 'level':
-        booksName.extend(os.listdir(Dir + '/' +  d))
-        for name in os.listdir(Dir + '/' + d):
-            booksDir.append(d + '/' + name)
-fre1 = []
-fre2 = []
-for eachword in re.findall(r"[a-zA-Z]+", open(booksDir[41]).read().lower()):
-    print eachword
-    #answer = raw_input()
-    #fre1.append(int(answer))
-    #os.system("./fakeysyprog " + eachword)
-
-#for eachword in re.findall(r"[a-zA-Z]+", open(booksDir[-1]).read().lower()):
-    #print eachword
-    #answer = raw_input()
-    #fre2.append(int(answer))
-    #os.system("./fakeysyprog " + eachword)
-print 'done!'
-#plt.ylabel('frequency')
-#plt.xlabel('words in book')
-#plt.plot(fre1, label="level1")
-#plt.plot(fre2, label="level6")
-#plt.legend()
-#plt.show()
+array = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+for each in num:
+    array[each] += 1
+s = 0
+for each in array:
+    s += each
+for i in range(0, len(array)):
+    array[i] = (array[i] + 0.0) / s 
+print s, array
